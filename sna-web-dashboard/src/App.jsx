299660c600500b68fd2a96ad64e1c2f5d2314944
Billie <130059458+Shogiita@users.noreply.github.com>
2026-04-21 15:00:59 +0700

    deploy

--- sna-web-dashboard/src/App.jsx
+++ sna-web-dashboard/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Network } from 'vis-network';
 
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = 'https://backendsna-py-387510652840.asia-southeast2.run.app';
 
 // --- Kumpulan Ikon ---
 const IconNetwork = () => (
